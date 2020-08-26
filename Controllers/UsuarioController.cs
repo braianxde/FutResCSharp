a7ef55cbf185f68c3af9040990d30dead7fd61a1
Braian Wegmann <braianwegmann@hotmail.com>
2020-08-26 18:24:44 -0300

    adicionado novos metodos classe usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -57,33 +57,70 @@ namespace ProjetoIntegrador4A.Controllers
         [Route("AlterarUsuario")]
         public string AlterarUsuario(Usuario usuario)
         {
+            try
+            {
+                //QUERY SQL
+                cmd.CommandText = "update usuario set nome = @nome, email = @email, senha = @senha, token = @token where id = @id";
 
-            return "Usuário alterado com sucesso!";
+                //VINCULA O QUE FOI RECEBIDO NO WEBSERVICE EM BINDS PARA TROCAR NA QUERY
+                cmd.Parameters.AddWithValue("@id", usuario.Id);
+                cmd.Parameters.AddWithValue("@nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@email", usuario.Email);
+                cmd.Parameters.AddWithValue("@senha", usuario.Senha);
+                cmd.Parameters.AddWithValue("@token", usuario.Token);
+
+                //SE CONECTA NO BANCO
+                cmd.Connection = conexao.conectar();
+                //EXECUTA A QUERY
+                cmd.ExecuteNonQuery();
+                //DESCONECTA NO BANCO
+                conexao.desconectar();
+
+                return "Usuário alterado com sucesso!";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
         }
 
         [AcceptVerbs("DELETE")]
-        [Route("ExcluirUsuario/{codigo}")]
-        public string ExcluirUsuario(int codigo)
+        [Route("ExcluirUsuario/{id}")]
+        public string ExcluirUsuario(string Id)
         {
-            return "Registro excluido com sucesso!";
+            try
+            {
+                //QUERY SQL
+                cmd.CommandText = "delete from usuario where id = @id";
+
+                //VINCULA O QUE FOI RECEBIDO NO WEBSERVICE EM BINDS PARA TROCAR NA QUERY
+                cmd.Parameters.AddWithValue("@id",Id);
+
+                //SE CONECTA NO BANCO
+                cmd.Connection = conexao.conectar();
+                //EXECUTA A QUERY
+                cmd.ExecuteNonQuery();
+                //DESCONECTA NO BANCO
+                conexao.desconectar();
+
+                return "Usuário excluido com sucesso!";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
         }
 
         [AcceptVerbs("GET")]
-        [Route("ConsultarUsuarioPorCodigo/{codigo}")]
-        public void ConsultarUsuarioPorCodigo(int codigo)
-        {
-            return ;
-        }
-
-        [AcceptVerbs("GET")]
-        [Route("ConsultarUsuarios")]
-        public String ConsultarUsuarios()
+        [Route("ConsultarUsuarioPorCodigo/{id}")]
+        public string ConsultarUsuarioPorCodigo(int Id)
         {
             try
             {
                 List<Usuario> users = new List<Usuario>();
                 int varint = 0;
-                cmd.CommandText = "SELECT * FROM usuario ORDER BY ID DESC";
+                cmd.CommandText = "SELECT * FROM usuario where id = @id";
+                cmd.Parameters.AddWithValue("@id", Id);
                 cmd.Connection = conexao.conectar();
                 MySqlDataReader reader = cmd.ExecuteReader();
 
@@ -97,7 +134,8 @@ namespace ProjetoIntegrador4A.Controllers
 
                     reader.NextResult();
                 }
-                
+
+                reader.Close();
                 return JsonConvert.SerializeObject(users, Formatting.Indented);
 
             }
@@ -110,7 +148,47 @@ namespace ProjetoIntegrador4A.Controllers
                 conexao.desconectar();
             }
 
-            return "Usuário cadastrado com sucesso!";
+            return "Nenhum usuario encontrado";
+
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarUsuarios")]
+        public String ConsultarUsuarios()
+        {
+            try
+            {
+                List<Usuario> users = new List<Usuario>();
+                int varint = 0;
+                cmd.CommandText = "SELECT * FROM usuario";
+                cmd.Connection = conexao.conectar();
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        varint++;
+                        users.Add(new Usuario(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3), reader.GetString(4)));
+                    }
+
+                    reader.NextResult();
+                }
+
+                reader.Close();
+                return JsonConvert.SerializeObject(users, Formatting.Indented);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexao.desconectar();
+            }
+
+            return "Nenhum usuario encontrado";
 
         }
     }
