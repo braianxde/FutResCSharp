6ca892fcc3b05c3c34323f12227233718c27ff73
Braian Wegmann <braianwegmann@hotmail.com>
2020-08-26 09:33:34 -0300

    inicio

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoIntegrador4A.Model;
+
+
+namespace ProjetoIntegrador4A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private static List<UsuarioModel> listaUsuarios = new List<UsuarioModel>();
+
+        [AcceptVerbs("POST")]
+        [Route("CadastrarUsuario")]
+        public string CadastrarUsuario(UsuarioModel usuario)
+        {
+
+            listaUsuarios.Add(usuario);
+
+            return "Usuário cadastrado com sucesso!";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("AlterarUsuario")]
+        public string AlterarUsuario(UsuarioModel usuario)
+        {
+
+            listaUsuarios.Where(n => n.Codigo == usuario.Codigo)
+                         .Select(s =>
+                         {
+                             s.Codigo = usuario.Codigo;
+                             s.Login = usuario.Login;
+                             s.Nome = usuario.Nome;
+
+                             return s;
+
+                         }).ToList();
+
+
+
+            return "Usuário alterado com sucesso!";
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("ExcluirUsuario/{codigo}")]
+        public string ExcluirUsuario(int codigo)
+        {
+
+            UsuarioModel usuario = listaUsuarios.Where(n => n.Codigo == codigo)
+                                                .Select(n => n)
+                                                .First();
+
+            listaUsuarios.Remove(usuario);
+
+            return "Registro excluido com sucesso!";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarUsuarioPorCodigo/{codigo}")]
+        public UsuarioModel ConsultarUsuarioPorCodigo(int codigo)
+        {
+
+            UsuarioModel usuario = listaUsuarios.Where(n => n.Codigo == codigo)
+                                                .Select(n => n)
+                                                .FirstOrDefault();
+
+            return usuario;
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultarUsuarios")]
+        public List<UsuarioModel> ConsultarUsuarios()
+        {
+            return listaUsuarios;
+        }
+    }
+}
---
+++ Model/UsuarioModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoIntegrador4A.Model
+{
+    public class UsuarioModel
+    {
+        private int codigo;
+        private string nome;
+        private string login;
+
+        public UsuarioModel()
+        {
+        }
+
+        public UsuarioModel(int codigo, string nome, string login)
+        {
+            this.Codigo = codigo;
+            this.Nome = nome;
+            this.Login = login;
+        }
+
+        public int Codigo
+        {
+            get
+            {
+                return codigo;
+            }
+
+            set
+            {
+                codigo = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+    }
+}
