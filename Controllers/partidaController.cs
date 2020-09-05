0fbede532a192fb22510d164eeac51f339905878
Braian Wegmann <braianwegmann@hotmail.com>
2020-09-05 10:35:57 -0300

    Criado partida controller e model

---
+++ Controllers/partidaController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using ProjetoIntegrador4A.Models;
+
+namespace ProjetoIntegrador4A.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class partidaController : ControllerBase
+    {
+        Conexao conexao = new Conexao();
+        MySqlCommand cmd = new MySqlCommand();
+
+        [AcceptVerbs("POST")]
+
+        [Route("InserirPartida")]
+        public string InserirPartida(partida partida)
+        {
+            try
+            {
+                cmd.CommandText = "insert into partida (id, data_hora, id_mandante, id_visitante, id_rodada, gols_visitante, gols_mandante) values (@id, STR_TO_DATE(@data_hora,'%d/%m/%Y %h:%i:%s'), @id_mandante, @id_visitante, @id_rodada, @gols_visitante, @gols_mandante)";
+
+                cmd.Parameters.AddWithValue("@id", partida.Id);
+                cmd.Parameters.AddWithValue("@data_hora", partida.Data_hora);
+                cmd.Parameters.AddWithValue("@id_mandante", partida.Id_mandante);
+                cmd.Parameters.AddWithValue("@id_visitante", partida.Id_visitante);
+                cmd.Parameters.AddWithValue("@id_rodada", partida.Id_rodada);
+                cmd.Parameters.AddWithValue("@gols_visitante", partida.Gols_mandante);
+                cmd.Parameters.AddWithValue("@gols_mandante", partida.Gols_visitante);
+
+                cmd.Connection = conexao.conectar();
+
+                cmd.ExecuteNonQuery();
+
+                conexao.desconectar();
+
+                return "Partida inserida com sucesso";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("ConsultaPartidasPorRodada/{id_rodada}")]
+        public string ConsultaPartidasPorRodada(int id_rodada)
+        {
+            try
+            {
+                List<partida> partidas = new List<partida>();
+                int varint = 0;
+                cmd.CommandText = "select p.id, p.data_hora, gols_mandante, gols_visitante, p.id_rodada, d.nome as nome_visitante , c.nome as nome_mandante, c.imagem as imagem_mandante, d.imagem as imagem_visitante from partida as p join clube as c on p.id_mandante = c.id join clube as d on p.id_visitante = d.id and id_rodada = @id_rodada";
+                cmd.Parameters.AddWithValue("@id_rodada", id_rodada);
+                cmd.Connection = conexao.conectar();
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        varint++;
+                        partidas.Add(new partida(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetInt32(4), reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetString(8)));
+                    }
+                    reader.NextResult();
+                }
+                reader.Close();
+                return JsonConvert.SerializeObject(partidas, Formatting.Indented);
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conexao.desconectar();
+            }
+            return "Rodada sem partidas!";
+
+        }
+    }
+}
--- Models/partida.cs
+++ Models/partida.cs
@@ -7,7 +7,52 @@ namespace ProjetoIntegrador4A.Models
 {
     public class partida
     {
-        public partida(int id, DateTime data_hora, int id_mandante, int id_visitante, int gols_mandante, int gols_visitante, int id_rodada)
+        private int id;
+        private string data_hora;
+        private int id_mandante;
+        private int id_visitante;
+        private int gols_mandante;
+        private int gols_visitante;
+        private int id_rodada;
+        private string nome_visitante;
+        private string nome_mandante;
+        private string imagem_visitante;
+        private string imagem_mandante;
+
+        public partida()
+        {
+
+        }
+
+        public partida(int id, string data_hora, int id_mandante, int id_visitante, int gols_mandante, int gols_visitante, int id_rodada, string nome_visitante, string nome_mandante, string imagem_visitante, string imagem_mandante)
+        {
+            this.id = id;
+            this.data_hora = data_hora;
+            this.id_mandante = id_mandante;
+            this.id_visitante = id_visitante;
+            this.gols_mandante = gols_mandante;
+            this.gols_visitante = gols_visitante;
+            this.id_rodada = id_rodada;
+            this.nome_visitante = nome_visitante;
+            this.nome_mandante = nome_mandante;
+            this.imagem_visitante = imagem_visitante;
+            this.imagem_mandante = imagem_mandante;
+        }
+
+        public partida(int id, string data_hora, int gols_mandante, int gols_visitante, int id_rodada, string nome_visitante, string nome_mandante, string imagem_mandante, string imagem_visitante)
+        {
+            this.id = id;
+            this.data_hora = data_hora;
+            this.gols_mandante = gols_mandante;
+            this.gols_visitante = gols_visitante;
+            this.id_rodada = id_rodada;
+            this.nome_visitante = nome_visitante;
+            this.nome_mandante = nome_mandante;
+            this.imagem_visitante = imagem_visitante;
+            this.imagem_mandante = imagem_mandante;
+        }
+
+        public partida(int id, string data_hora, int id_mandante, int id_visitante, int gols_mandante, int gols_visitante, int id_rodada)
         {
             this.id = id;
             this.data_hora = data_hora;
@@ -18,12 +63,147 @@ namespace ProjetoIntegrador4A.Models
             this.id_rodada = id_rodada;
         }
 
-        public int id { get; set; }
-        public DateTime data_hora { get; set; }
-        public int id_mandante { get; set; }
-        public int id_visitante { get; set; }
-        public int gols_mandante { get; set; }
-        public int gols_visitante { get; set; }
-        public int id_rodada { get; set; }
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Data_hora
+        {
+            get
+            {
+                return data_hora;
+            }
+
+            set
+            {
+                data_hora = value;
+            }
+        }
+
+        public int Id_mandante
+        {
+            get
+            {
+                return id_mandante;
+            }
+
+            set
+            {
+                id_mandante = value;
+            }
+        }
+
+        public int Id_rodada
+        {
+            get
+            {
+                return id_rodada;
+            }
+
+            set
+            {
+                id_rodada = value;
+            }
+        }
+
+        public int Id_visitante
+        {
+            get
+            {
+                return id_visitante;
+            }
+
+            set
+            {
+                id_visitante = value;
+            }
+        }
+
+        public int Gols_visitante
+        {
+            get
+            {
+                return gols_visitante;
+            }
+
+            set
+            {
+                gols_visitante = value;
+            }
+        }
+
+        public int Gols_mandante
+        {
+            get
+            {
+                return gols_mandante;
+            }
+
+            set
+            {
+                gols_mandante = value;
+            }
+        }
+
+        public string Nome_visitante
+        {
+            get
+            {
+                return nome_visitante;
+            }
+
+            set
+            {
+                nome_visitante = value;
+            }
+        }
+
+        public string Nome_mandante
+        {
+            get
+            {
+                return nome_mandante;
+            }
+
+            set
+            {
+                nome_mandante = value;
+            }
+        }
+
+        public string Imagem_visitante
+        {
+            get
+            {
+                return imagem_visitante;
+            }
+
+            set
+            {
+                imagem_visitante = value;
+            }
+        }
+
+        public string Imagem_mandante
+        {
+            get
+            {
+                return imagem_mandante;
+            }
+
+            set
+            {
+                imagem_mandante = value;
+            }
+        }
     }
 }
