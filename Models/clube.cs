0205f255d84f52783ce949c0e92b177a9695e3b0
marciobecker <ml.becker@hotmail.com>
2020-09-05 11:31:48 -0300

    Criado controlles clube e Jogador

--- Models/clube.cs
+++ Models/clube.cs
@@ -10,6 +10,21 @@ namespace ProjetoIntegrador4A.Models
 {
     public class clube
     {
+        private int id;
+        private string nome;
+        private string cidade;
+        private int pontos;
+        private int vitorias;
+        private int empates;
+        private int derrotas;
+        private int gols_pro;
+        private int gols_contra;
+        private string nome_tecnico;
+        private string imagem;
+
+        public clube ()
+        { }
+
         public clube(int id, string nome, string cidade, int pontos, int vitorias, int empates, int derrotas, int gols_pro, int gols_contra, string nome_tecnico, string imagem)
         {
             this.id = id;
@@ -25,16 +40,16 @@ namespace ProjetoIntegrador4A.Models
             this.imagem = imagem;
         }
 
-        public int id { get; set; }
-        public string nome { get; set; }
-        public string cidade { get; set; }
-        public int pontos { get; set; }
-        public int vitorias { get; set; }
-        public int empates { get; set; }
-        public int derrotas { get; set; }
-        public int gols_pro { get; set; }
-        public int gols_contra { get; set; }
-        public string nome_tecnico { get; set; }
-        public string imagem { get; set; }
+        public int Id { get { return id; } set { id = value; } }
+        public string Nome { get { return nome; } set { nome = value; } }
+        public string Cidade { get { return cidade; } set { cidade = value; } }
+        public int Pontos { get { return pontos; } set { pontos = value; } }
+        public int Vitorias { get { return vitorias; } set { vitorias = value; } }
+        public int Empates { get { return empates; } set { empates = value; } }
+        public int Derrotas { get { return derrotas; } set { derrotas = value; } }
+        public int Gols_pro { get { return gols_pro; } set { gols_pro = value; } }
+        public int Gols_contra { get { return gols_contra; } set { gols_contra = value; } }
+        public string Nome_tecnico { get { return nome_tecnico; } set { nome_tecnico = value; } }
+        public string Imagem { get { return imagem; } set { imagem = value; } }
     }
 }
